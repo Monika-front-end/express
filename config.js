003987a7f7973a3e13f610bd13976f0d6c3586c7
Monika Ludwikowska <monikaagaludwikowska79@gmail.com>
2020-03-27 12:10:26 +0100

    news filter front

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // db:
-  //   'mongodb+srv://admin:2DyCAnn4ClMZ8DTc@cluster0-cutul.mongodb.net/test?retryWrites=true',
-  db: '',
+  db:
+    'mongodb+srv://admin:2DyCAnn4ClMZ8DTc@cluster0-cutul.mongodb.net/test?retryWrites=true',
+  // db: '',
   keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
